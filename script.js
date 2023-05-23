530ae95b56e518f1b4aba29bd6bb629b1e6d662f
Marianne Shaffer <shafferma@pcsb.org>
2023-05-23 08:04:05 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: 
+    Date:   
+
+    Filename: project01-04.js
+*/
+
+//define variables for home and work addresses
+
+let homeStreet = "1 Main St.";
+let homeCity = "Sicilia";
+let homeState = "MA";
+let homeCode = "02103";
+
+let workStreet = "15 Oak Ln.", workCity = "Central City", workState = "MA", workCode = "02104";
